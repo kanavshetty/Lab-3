dd75ab4996b9b5294e8da0758e2b714529cfa0b1
Kanav Shetty <shettykv@mail.uc.edu>
2024-01-29 14:55:32 -0500

    Updated llh.h

--- llh.h
+++ llh.h
@@ -1,13 +1,23 @@
 #include<iostream>
 class LLH{
 private:
-    int  little = 0;
-    int lot = 0;
-    int heap = 0;
+    int  littles = 0;
+    int lots = 0;
+    int heaps = 0;
 
 public:
     LLH(){};
-    LLH(int little, int lot, int heap){
-        
+    LLH(int littles, int lots, int heaps){
+        if(littles>7){
+            lots += littles/7;
+            littles %= 7;
+        }
+        littles += littles;
+        if(lots>23){
+            heaps += lots/7;
+            lots %= lots;
+        }
+        lots += lots;
+        heaps += heaps;
     }
 };

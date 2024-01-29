1191dbc59753efbd12caa0616cad9e1c965a49a9
Kanav Shetty <shettykv@mail.uc.edu>
2024-01-29 14:46:00 -0500

    New llh.h file

---
+++ llh.h
@@ -0,0 +1,13 @@
+#include<iostream>
+class LLH{
+private:
+    int  little = 0;
+    int lot = 0;
+    int heap = 0;
+
+public:
+    LLH(){};
+    LLH(int little, int lot, int heap){
+        
+    }
+};

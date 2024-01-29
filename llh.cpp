acf4a4e7e7ae4424e7797403b7574c62b37f6f6d
RonVTR <106887946+RonVTR@users.noreply.github.com>
2024-01-29 15:16:35 -0500

    Update llh.cpp
    added default constructor and littles overloaded constructor

--- llh.cpp
+++ llh.cpp
@@ -1,2 +1,21 @@
 #include "llh.h"
 
+// Default constructor
+llh::llh() : littles(0), lots(0), heaps(0) {}
+
+
+// Overloaded constructor for "littles" attribute
+llh::llh(int littles) {
+    if (littles > 7) {
+        lots += littles / 7;
+        this->littles = littles % 7;
+    } else {
+        this->littles = littles;
+    }
+
+    if (lots > 23) {
+        heaps += lots / 7;
+        lots %= 7;
+    }
+}
+
